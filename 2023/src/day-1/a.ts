45fe03c55693e3ec6927557f1f36964b6c88038b
Audi <audiwu321@gmail.com>
2023-12-01 23:11:11 +0800

    feat: day 1

--- 2023/src/day-1/a.ts
+++ 2023/src/day-1/a.ts
@@ -4,34 +4,47 @@ import chalk from 'chalk';
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
   const arrayOfNumberArrays = turnIntoArrayOfNumberArrays(data);
-  const maxSum = arrayOfNumberArrays.map(getSum).reduce(max, 0);
-  return maxSum;
-}
-
-function max(accumulator: number, currentValue: number): number {
-  return Math.max(accumulator, currentValue);
+  const total = getSum(arrayOfNumberArrays);
+  return total;
 }
 
 export function getSum(input: number[]): number {
   return input.reduce((acc, curr) => acc + curr, 0);
 }
 
-export function turnIntoArrayOfNumberArrays(data: string[]): number[][] {
-  const toReturn = [[]];
+export function turnIntoArrayOfNumberArrays(data: string[]): number[] {
+  const toReturn = [];
+
   for (const line of data) {
-    if (line === '') {
-      toReturn.push([]);
-    } else {
-      toReturn[toReturn.length - 1].push(+line);
-    }
+    const number = parseInt(getNumberFromString(line));
+    toReturn.push(number);
   }
+
   return toReturn;
 }
+
+export function getNumberFromString(line: string): string {
+  const numbers = [];
+
+  for (let i = 0; i < line.length; i++) {
+    const character = parseInt(line.charAt(i));
+
+    if (!isNaN(character)) {
+      numbers.push(character);
+    }
+  }
+
+  return `${numbers[0]}${numbers[numbers.length - 1]}`;
+}
+
+
+
+
 
 // don't change below this line
 // this makes sure we don't call the function when we import it for tests
 if (process.argv.includes('--run')) {
   day1a().then((answer) => {
-    console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+    console.log(chalk.bgGreen('Your Answer Part 1:'), chalk.green(answer));
   });
 }
--- 2023/src/day-1/b.ts
+++ 2023/src/day-1/b.ts
@@ -1,21 +1,63 @@
 import { readData } from '../utils';
 import chalk from 'chalk';
-import { getSum, turnIntoArrayOfNumberArrays } from './a';
+import { getSum } from './a';
+
+const NUMBERS = [
+  { word: 'one', value: 1 },
+  { word: 'two', value: 2 },
+  { word: 'three', value: 3 },
+  { word: 'four', value: 4 },
+  { word: 'five', value: 5 },
+  { word: 'six', value: 6 },
+  { word: 'seven', value: 7 },
+  { word: 'eight', value: 8 },
+  { word: 'nine', value: 9 },
+];
 
 export async function day1b(dataPath?: string) {
   const data = await readData(dataPath);
   const nums = turnIntoArrayOfNumberArrays(data);
-  const topThree = nums
-    .map(getSum)
-    .sort((a, b) => b - a)
-    .slice(0, 3);
-  return topThree.reduce((acc, curr) => acc + curr, 0);
+  const total = getSum(nums);
+  return total;
+}
+
+export function turnIntoArrayOfNumberArrays(data: string[]): number[] {
+  const toReturn = data.map((line) => {
+    const matchDigits = NUMBERS.map((number) => ({
+      ...number,
+      matches: Array.from(
+        line.matchAll(new RegExp(`${number.word}|${number.value}`, 'g'))
+      ),
+    }));
+
+    const filterZeroMatchesDigits = matchDigits.filter(
+      ({ matches }) => matches.length > 0
+    );
+
+    const transformDigitsIntoValueAndIndex = filterZeroMatchesDigits.flatMap(
+      ({ value, matches }) => {
+        return matches.map(({ index }) => ({ value, index }));
+      }
+    );
+
+    const sortDigitsByIndex = transformDigitsIntoValueAndIndex.sort(
+      (a, b) => a.index - b.index
+    );
+    
+    const first = sortDigitsByIndex[0];
+    const last = sortDigitsByIndex[sortDigitsByIndex.length - 1];
+    const digitsValue = Number(`${first.value}${last.value}`);
+
+    return digitsValue;
+  });
+
+  return toReturn;
 }
 
 // don't change below this line
 // this makes sure we don't call the function when we import it for tests
 if (process.argv.includes('--run')) {
   day1b().then((answer) => {
-    console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+    console.log(chalk.bgGreen('Your Answer Part 2:'), chalk.green(answer));
   });
 }
